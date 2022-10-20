036ac84c4bdfff32db3ad589003a6001e30ec3ce
GalileoCap <devel@galilecap.me>
2022-10-20 17:53:11 -0300

    EJ1: FIX: Disjoint justification

--- src/ej1/disjoint.h
+++ src/ej1/disjoint.h
@@ -5,14 +5,14 @@
 using DisjointSet = std::vector<int>;
 
 //U: Returns the group representative for x
-//Time complexity: O(alpha(|set|)) ~~~> O(1)
+//Time complexity: O(alpha(|set|)) ~~~> O(1), because |set| <= 10^4
 int find(
   DisjointSet& set,
   int x
 );
 
 //U: Joins two trees
-//Time complexity: O(alpha(|set|)) ~~~> O(1)
+//Time complexity: O(alpha(|set|)) ~~~> O(1), because |set| <= 10^4
 void join(
   DisjointSet& set,
   int x,
