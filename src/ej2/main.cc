e0e23a3d584bb1197c777d7be2cf31d12b48f082
GalileoCap <devel@galilecap.me>
2022-10-27 22:05:29 -0300

    EJ2: TRY: Max out

--- src/ej2/main.cc
+++ src/ej2/main.cc
@@ -3,27 +3,74 @@
 #include <iostream>
 
 struct DisjointSet {
+  struct Data {
+    int size;
+    std::vector<int> max;
+  };
+
   DisjointSet(int n);
 
   int unite(int v, int w);
   int find(int v);
 
+  int& size(int v);
+  int& max(int v, int w);
+
   std::vector<int> repr;
-  std::vector<int> size;
+  std::vector<struct Data> data;
 };
 
-DisjointSet::DisjointSet(int n) : repr(n, -1), size(n, 1) {}
+DisjointSet::DisjointSet(int n) :
+  repr(n, -1),
+  data(n, {1, std::vector<int>(n, -1)})
+  {}
+
+int& DisjointSet::size(int v) {
+  return data[find(v)].size;
+}
+
+int& DisjointSet::max(int v, int w) {
+  v = find(v); w = find(w);
+  int &mvw = data[v].max[w],
+      &mwv = data[w].max[v];
+  mvw = std::max(mvw, mwv);
+  mwv = std::max(mvw, mwv);
+  return mwv;
+}
 
 int DisjointSet::unite(int v, int w) {
   v = find(v); w = find(w);
-  if (v == w) return size[v];
-  if (size[v] < size[w]) std::swap(v, w);
+  if (v == w) return size(v); //A: Nothing to do
+  if (size(v) < size(w)) std::swap(v, w); //TODO: Is this needed?
+
+  size(v) += size(w);
+  for (int u = 0; u < repr.size(); u++)
+    max(v, u) = std::max(max(v, u), max(w, u));
   repr[w] = v;
-  return size[v] += size[w];
+
+  return size(v);
 }
 
 int DisjointSet::find(int v) {
   return (repr[v] == -1) ? v : repr[v] = find(repr[v]);
+}
+
+void printDisjoint(struct DisjointSet& set) {
+  printf("   ");
+  for (int v = 0; v < set.repr.size(); v++)
+    if (v == set.find(v)) printf(" %i ", v+1);
+  printf("\n");
+
+  for (int v = 0; v < set.repr.size(); v++) {
+    if (v == set.find(v)) {
+      printf("%i: ", v+1);
+      for (int w = 0; w <= v; w++)
+        if (v == w) printf(" - ");
+        else if (w == set.find(w)) printf("% i ", set.max(v, w));
+      printf("\n");
+    }
+  }
+  printf("\n");
 }
 
 int main(void) {
@@ -41,7 +88,11 @@ int main(void) {
       std::cin >> v >> w >> k;
       v--; w--; //NOTE: The input is 1-indexed
       edges[i] = {v, w, k};
+      set.max(v, w) = k;
     }
+#ifdef DEBUG
+    printDisjoint(set);
+#endif // DEBUG
 
     //A: Sort the edges by heaviest to lightest; O(m log(m)) <= O(m log(n^2)) = O(2m log(n)) = O(m log(n))
     std::sort(edges.begin(), edges.end(), Edge::greaterThan);
@@ -49,12 +100,17 @@ int main(void) {
     int res = 0;
     for (const struct Edge& e : edges) {
       int v = e.v, w = e.w, weight = e.weight;
+      bool counts = true;
       if (set.find(v) != set.find(w)) {
-        //TODO: if (weight > min (v, w) || ) {
-        res += set.unite(v, w);
-        //TODO: } Unite min v, w
+        int sum = set.unite(v, w);
+        for (int u = 0; u < n; u++)
+          if (set.find(u) != set.find(v))
+            counts &= weight > set.max(v, u);
+
+        res += sum * counts;
 #ifdef DEBUG
-        printf("(%i, %i): %i\n", v+1, w+1, res);
+        printf("(%i, %i): %C? %i\n", v+1, w+1, counts ? 'T' : 'F', set.unite(v, w));
+        printDisjoint(set);
 #endif // DEBUG
       }
     }
